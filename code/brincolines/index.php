032d48accd8a72d6c4d423995b5d50ab0f9ecc7c
benjamin137 <jvalenzuela1@ucol.mx>
2025-04-29 00:15:46 -0600

    Finished eventos

--- code/brincolines/index.php
+++ code/brincolines/index.php
@@ -1,6 +1,4 @@
 <?php
 
 //Redirigiar a brincolines/vistas/listar.php?tipo=eventos
-header("Location: ./brincolines/vistas/listar.php?tipo=eventos");
-
-?>
+header("Location: ./vistas/listar.php?tipo=eventos");
